a2c20d83620310331884a09ea0b5fb71514f7232
Антон Киршанов <maetchkin@gmail.com>
2019-05-09 02:44:25 +0300

    print headers

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello World!\n" + JSON.stringify(request.headers,null,2) );
+
 });
 
 const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+console.log(process);
