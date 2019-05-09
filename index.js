623d24f1b440d44f131dd6485b1bf3ce0f14ed41
Антон Киршанов <maetchkin@gmail.com>
2019-05-10 02:26:50 +0300

    add bcd-front deps

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
 const server = http.createServer(
-    
     (request, response) => {
         const name = request.headers['x-ms-client-principal-name'];
         const res  = "hello " +  name + "\n\n" + JSON.stringify(request.headers,null,2);
@@ -14,4 +13,3 @@ const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
-console.log(process);
